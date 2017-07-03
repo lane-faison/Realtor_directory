85569433861e3382696ef119c9840b0b0ae45347
Lane Faison <lane.faison@icloud.com>
2017-07-03 13:42:48 -0600

    TableView and TableViewCell UI

---
+++ Realtor Directory/DirectoryVC.swift
@@ -0,0 +1,20 @@
+//
+//  DirectoryVC.swift
+//  Realtor Directory
+//
+//  Created by Lane Faison on 7/3/17.
+//  Copyright © 2017 Lane Faison. All rights reserved.
+//
+
+import UIKit
+
+class DirectoryVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    
+    }
+
+}
+
