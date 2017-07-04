b034bc2fd06e53c704ba6f93a8858bba18d0bd8a
Lane Faison <lane.faison@icloud.com>
2017-07-03 23:36:14 -0600

    SwiftyJSON, Alamofire hooked up. Data is parsed and new realtors are appended into an array of all realtors

---
+++ Realtor Directory/RealtorCell.swift
@@ -0,0 +1,31 @@
+//
+//  RealtorCell.swift
+//  Realtor Directory
+//
+//  Created by Lane Faison on 7/3/17.
+//  Copyright © 2017 Lane Faison. All rights reserved.
+//
+
+import UIKit
+
+class RealtorCell: UITableViewCell {
+
+    @IBOutlet weak var view: UIView!
+    @IBOutlet weak var thumbPortrait: RoundedOutlineImage!
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var phoneNumber: UILabel!
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
